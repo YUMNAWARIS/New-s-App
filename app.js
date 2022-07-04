7dfc49a3a66a20a2c94245aea128907004a48689
Yumna Waris <yumnaahwaris@gmail.com>
2022-07-04 13:34:04 +0500

    Final

--- app.js
+++ app.js
@@ -8,73 +8,30 @@ let sources = "bbc"
 let country = "us"
 let lang = "en"
 let baseUrl = `http://api.mediastack.com/v1/news?access_key=${apiKey}&sources=${sources}&countries=${country}&languages=${lang}`
-let xhr =  new XMLHttpRequest();
-xhr.open("GET", baseUrl);
-xhr.send();
-xhr.onload=
-    ()=>{
-        let text = JSON.parse(xhr.responseText);
-        let info = text.pagination;
-        let newsList = text.data;
-        
-        if(info.count >= 6){
-            for (let i = 0; i < 6; i++) {
-                let news = newsList [i];
-                let body =  
-                            `
-                            <div class="card card-styling bg-dark  ">
-                                <div class="card-body">
-                                    <h5 class="card-title">${news.title}</h5>
-                                    <p class="card-text">${news.description}</p>
-                                </div>
-        
-                                <a href="${news.url}" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                            `
-                topHeadlinesItems += body;
-            }
-            topHeadlines.innerHTML = topHeadlinesItems;
-        }
-        else
-        {
-            newsList.forEach((news,index) => {
-            console.log(index+1, news)
-    
-            let body =  
-                        `
-                        <div class="card card-styling bg-dark  ">
-                            <div class="card-body">
-                                <h5 class="card-title">${news.title}</h5>
-                                <p class="card-text">${news.description}</p>
-                            </div>
-    
-                            <a href="${news.url}" class="btn btn-primary">Go somewhere</a>
+
+async function getNewsTopheadlines(url) {
+    const response = await fetch(url)
+    const data = await response.json();
+    return data;
+}
+getNewsTopheadlines(baseUrl).then((data) => {
+    let info = data.pagination;
+    let newsList = data.data;
+    newsList.forEach((news, index) => {
+        console.log(index + 1, news)
+        createNewsElement(news)
+    })
+    topHeadlines.innerHTML = topHeadlinesItems; 
+}).catch(e => console.log("Sorry Cant process your request due to network error...", e));
+function createNewsElement(news) {
+    let body = `   <div class="card card-styling bg-dark  ">
+                        <div class="card-body">
+                            <h5 class="card-title">${news.title}</h5>
+                            <p class="card-text">${news.description}</p>
                         </div>
-                        `
-
-    
-                    // `<div class="accordion-item">
-                    //     <h2 class="accordion-header" id="news${index+1}">
-                    //     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    //         data-bs-target="#collapse${index+1}" aria-expanded="true" aria-controls="collapse${index+1}">
-                    //         <strong>${news.title}</strong>
-                    //     </button>
-                    //     </h2>
-                    //     <div id="collapse${index+1}" class="accordion-collapse collapse" aria-labelledby="news${index+1}"
-                    //     data-bs-parent="#topHeadlines">
-                    //     <div class="accordion-body">
-                    //         ${news.description}
-                            
-                    //     </div>
-                    //     </div>
-                    // </div>` 
-    
-    
-            topHeadlinesItems += body;
-            });
-            topHeadlines.innerHTML = topHeadlinesItems;
-        }
-
-    }
-
+                        <a href="${news.url}" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                `
+    topHeadlinesItems += body;
+}
 
